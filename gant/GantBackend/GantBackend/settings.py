89099cb6ca3791a3bc26e41e890ede30ad896600
awsoremod <sotnikovilia@yandex.ru>
2023-05-28 18:37:27 +0500

    убрал сессии ещё в middleware

--- gant/GantBackend/GantBackend/settings.py
+++ gant/GantBackend/GantBackend/settings.py
@@ -35,7 +35,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    #'django.contrib.sessions.middleware.SessionMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
