3c26f748c811fadf2b8d15551d6c6e5d0ed81a30
sxkxixx <sasha.kornilov.1212@gmail.com>
2023-05-28 18:08:55 +0500

    csrf origins

--- gant/GantBackend/GantBackend/settings.py
+++ gant/GantBackend/GantBackend/settings.py
@@ -15,7 +15,7 @@ DEBUG = int(os.getenv('DEBUG', True))
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS').split()
 
 # Application definition
 
