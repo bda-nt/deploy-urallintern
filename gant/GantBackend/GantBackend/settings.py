bf438d4c3310b9870b2ce9f7cb8d35f893ec3913
awsoremod <sotnikovilia@yandex.ru>
2023-05-28 18:43:16 +0500

    fix

--- gant/GantBackend/GantBackend/settings.py
+++ gant/GantBackend/GantBackend/settings.py
@@ -35,9 +35,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    #'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     # 'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
